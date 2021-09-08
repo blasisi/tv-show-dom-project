60dbf06f477daeca957c123de5a49d93c00c25a2
blasisi <Bimbolamarialasisi@gmail.com>
2021-09-08 01:30:11 +0100

    started working on level 200

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //You can edit ALL of the code here
+let allEpisodes = [];
 function setup() {
-  const allEpisodes = getAllEpisodes();
+   allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-// Global variables
+// lv
 const rootElem = document.getElementById("root");
 const heading = document.createElement("h1");
 const image = document.createElement("img");
 const epSeason= document.createElement("p");
 const summary =document.createElement("div");
-const links =document.createElement("url");
 
 
 function makePageForEpisodes(episodeList) {
   
   const rootElem = document.getElementById("root");
+  rootElem.innerHTML= "";
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   episodeList.forEach((title) => {
     const heading = document.createElement("h1");
@@ -31,15 +32,11 @@ function makePageForEpisodes(episodeList) {
     summary.innerText = title.summary;
     rootElem.append(heading,image,epSeason,summary);
 
-  }
-}
-
 //  combine season number and episode number into an episode code:
-    const combineEp =document.createElement("select");
 
 // Your page should state somewhere that the data has (originally) come from TVMaze.com, and link back to that site (or the specific episode on that site). See tvmaze.com/api#licensing.
-    `
-
+    
+  
 
     
 
@@ -47,3 +44,20 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+function search(input){
+  let allInput = allEpisodes.filter(episode =>{
+    
+    return (episode.name).toLowerCase().includes(input.toLowerCase()) || (episode.summary).toLowerCase().includes(input.toLowerCase());
+    
+  })
+
+  makePageForEpisodes(allInput);
+
+}
+let searchInput = document.getElementById("search");
+searchInput.addEventListener( "input", ()=> {
+let input = searchInput.value;
+search(input);
+})
+
