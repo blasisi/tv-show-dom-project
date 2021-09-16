5a600f73c795bfab064799e0b1515abc7d96e836
blasisi <Bimbolamarialasisi@gmail.com>
2021-09-16 01:38:02 +0100

    redoing the html, css and javscript

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 //You can edit ALL of the code here
+// Gloval variable
+let dropDown = document.getElementById("listEpisodes");
+const rootElem = document.getElementById("root");
 let allEpisodes = [];
+
+// const container = document.getElementById("container");
+// const option = document.getCreateElement("option");
+// let searchInput = document.getElementById("SearchInput");
+// let searchResult = document.getElementById("ResultSearch");
+
+
 function setup() {
    allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-}
+  let allShows =getAllShows().sort((a,b) =>{
+    if (a.name.toLowerCase() > b.name.toLowerCase()){
+      return 1;
+    }else if (b.name.toLowerCase() > a.name.toLowerCase()) {
+      return 1;
+    }else{
+      return 0;
+    }
+  });
 
-// lv
-const rootElem = document.getElementById("root");
+
+
+// level 100
 const heading = document.createElement("h1");
 const image = document.createElement("img");
 const epSeason= document.createElement("p");
@@ -20,7 +39,7 @@ function makePageForEpisodes(episodeList) {
  const epCount = document.createElement("div");
  epCount.classList.add("episode_count");
 
-  epCount.textContent = `Got ${episodeList.length} episode(s)`;
+  // epCount.textContent = `Got ${episodeList.length} episode(s)`;
   rootElem.append(epCount);
 
   episodeList.forEach((title) => {
@@ -33,8 +52,6 @@ function makePageForEpisodes(episodeList) {
 
     const epSeason= document.createElement("p");
     epSeason.innerText = `S${title.season.toString().padStart(2,0)} E${title.number.toString().padStart(2,0)}`;
-    // padStrart is to number= 01, parseInt from string to number.
-
     const summary = document.createElement("div");
     summary.innerText = title.summary;
     movieCard.append(heading,image,epSeason,summary);
@@ -44,11 +61,10 @@ function makePageForEpisodes(episodeList) {
 
 // Your page should state somewhere that the data has (originally) come from TVMaze.com, and link back to that site (or the specific episode on that site). See tvmaze.com/api#licensing.
     
+
   
-
-    
-
    });
+   //level 300
 }
 
 window.onload = setup();
@@ -67,5 +83,8 @@ let searchInput = document.getElementById("search");
 searchInput.addEventListener( "input", ()=> {
 let input = searchInput.value;
 search(input);
+let option =document.createElement("option");
+option.text ="text";
+option.value ="myvalue";
 })
 
